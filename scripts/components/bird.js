4ac2e7886c2fb9db3818537dfcd76bae06bd888f
K3rill <razdor.2019@mail.ru>
2024-09-11 15:10:23 +0300

    bird

--- scripts/components/bird.js
+++ scripts/components/bird.js
@@ -1,4 +1,36 @@
-export class Bird {
+export default class Bird {
     constructor() {
+      this.x = 50;
+      this.y = 150;
+      this.width = 34;
+      this.height = 24;
+      this.velocity = 0;
+      this.gravity = 0.5;
+      this.jumpForce = -10;
+      this.sprite = new Image();
+      this.sprite.src = 'assets/img/bird.png';
+    }
+  
+    update() {
+      this.velocity += this.gravity;
+      this.y += this.velocity;
+  
+      if (this.y < 0) {
+        this.y = 0;
+        this.velocity = 0;
       }
-}
+    }
+  
+    jump() {
+      this.velocity = this.jumpForce;
+    }
+  
+    render(ctx) {
+      ctx.drawImage(this.sprite, this.x, this.y, this.width, this.height);
+    }
+  
+    reset() {
+      this.y = 150;
+      this.velocity = 0;
+    }
+  }
