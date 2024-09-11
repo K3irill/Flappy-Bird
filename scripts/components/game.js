494601fc66a12c5260bde368e8b8ec0908ab80ca
K3rill <razdor.2019@mail.ru>
2024-09-11 15:06:45 +0300

    main js components

--- scripts/components/game.js
+++ scripts/components/game.js
@@ -1,1 +1,13 @@
-'use strict'
+"use strict";
+import Bird from './bird.js';
+import Pipe from './pipe.js';
+import ScoreManager from './scoreManager.js';
+
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+
+const bird = new Bird();
+const pipes = [];
+let frame = 0;
+const scoreManager = new ScoreManager();
+let isGameOver = false;
