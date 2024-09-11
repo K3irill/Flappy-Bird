389d1ac48b8a616f4c776dbe893338c40c2fae28
K3rill <razdor.2019@mail.ru>
2024-09-11 15:12:18 +0300

    pipe

--- scripts/components/pipe.js
+++ scripts/components/pipe.js
@@ -1,4 +1,43 @@
-export class Pipe {
-    constructor() {
-      }
+export default class Pipe {
+  constructor(x, canvas) {
+    this.x = x;
+    this.width = 68;
+    this.gapHeight = 200;
+    this.speed = 2;
+    this.passed = false;
+    this.topHeight = Math.random() * (canvas.height / 2) + 50; 
+    this.bottomHeight = canvas.height - this.topHeight - this.gapHeight; 
+    this.sprite = new Image();
+    this.sprite.src = "assets/img/pipe.png";
+  }
+
+  update() {
+    this.x -= this.speed;
+  }
+
+  render(ctx) {
+    ctx.drawImage(this.sprite, this.x, 0, this.width, this.topHeight);
+
+    ctx.drawImage(
+      this.sprite,
+      this.x,
+      ctx.canvas.height - this.bottomHeight,
+      this.width,
+      this.bottomHeight
+    );
+  }
+
+  checkCollision(bird, canvasHeight) {
+    const hitTopPipe =
+      bird.x + bird.width > this.x &&
+      bird.x < this.x + this.width &&
+      bird.y < this.topHeight;
+
+    const hitBottomPipe =
+      bird.x + bird.width > this.x &&
+      bird.x < this.x + this.width &&
+      bird.y + bird.height > canvasHeight - this.bottomHeight;
+
+    return hitTopPipe || hitBottomPipe;
+  }
 }
